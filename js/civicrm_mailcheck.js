c88778a2ea36fe0eaaeedecb6d19796c3ca2989b
Andreas Hennings <andreas@dqxtech.net>
2012-05-21 18:06:25 +0200

    more js work, remove console.log

--- js/civicrm_mailcheck.js
+++ js/civicrm_mailcheck.js
@@ -3,7 +3,6 @@
   // var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
   var $msgbox = $('<div id="civicrm_mailcheck-msg">Did you mean <a href="#civicrm_mailcheck-msg"></a>?</div>');
   var $msgbox_link = $msgbox.find('a');
-  console.log($msgbox.html());
   var suggestion_full = null;
   $msgbox_link.click(function(){
     if (input_with_msgbox && suggestion_full) {
@@ -18,26 +17,21 @@
 
   Drupal.behaviors.civicrm_mailcheck = {
     attach: function(context, settings) {
-      console.log('attach', settings);
       for (var k in settings.civicrm_mailcheck.email_fields) {
-        var selector = '[name="' + k + '"]';
-        var $field = $(selector);
-        console.log(selector, $field);
-        $('#' + k).blur(function() {
-          console.log(this);
+        var selector = '#' + k.replace('][', '_').replace(']', '').replace('[', '_');
+        $(selector, context).blur(function() {
+          if ($(this).attr('name') != k) {
+            return;
+          }
           $(this).mailcheck({
             // domains: domains,   // optional
             suggested: function(element, suggestion) {
-              // callback code
-              console.log($(element).val(), suggestion, suggestion.full);
               $msgbox_link.html(suggestion.address + '@<strong>' + suggestion.domain + '</strong>');
               $(element).after($msgbox);
               input_with_msgbox = element[0];
               suggestion_full = suggestion.full;
             },
             empty: function(element) {
-              console.log('empty', element[0], input_with_msgbox);
-              // callback code
               if (input_with_msgbox == element[0]) {
                 $msgbox.remove();
                 input_with_msgbox = null;
