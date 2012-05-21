bd4dc9003a1cce1974f59b29c87ad86d49466c4a
Andreas Hennings <andreas@dqxtech.net>
2012-05-21 16:46:21 +0200

    initial commit d7 version

---
+++ js/civicrm_mailcheck.js
@@ -0,0 +1,52 @@
+(function($){
+
+  var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
+  var $msgbox = $('<div id="civicrm_mailcheck-msg">');
+  var $msgbox_link = $('<a href="javascript:(function(){})();">').appendTo($msgbox);
+  $msgbox.prepend('Did you mean ');
+  $msgbox.append(' ?');
+  console.log($msgbox.html());
+  var suggestion_full = null;
+  $msgbox_link.click(function(){
+    if (input_with_msgbox && suggestion_full) {
+      $(input_with_msgbox).val(suggestion_full);
+      $msgbox.remove();
+      input_with_msgbox = null;
+      suggestion_full = null;
+    }
+  });
+  var input_with_msgbox = null;
+
+  Drupal.behaviors.civicrm_mailcheck = {
+    attach: function(context, settings) {
+      console.log('attach', settings);
+      for (var k in settings.civicrm_mailcheck.email_fields) {
+        var $field = $('#' + k);
+        console.log($field);
+        $('#' + k).blur(function() {
+          console.log(this);
+          $(this).mailcheck({
+            // domains: domains,   // optional
+            suggested: function(element, suggestion) {
+              // callback code
+              console.log($(element).val(), suggestion, suggestion.full);
+              $msgbox_link.html(suggestion.address + '@<strong>' + suggestion.domain + '</strong>');
+              $(element).after($msgbox);
+              input_with_msgbox = element[0];
+              suggestion_full = suggestion.full;
+            },
+            empty: function(element) {
+              console.log('empty', element[0], input_with_msgbox);
+              // callback code
+              if (input_with_msgbox == element[0]) {
+                $msgbox.remove();
+                input_with_msgbox = null;
+                suggestion_full = null;
+              }
+            }
+          });
+        });
+      }
+    }
+  };
+})(jQuery);
